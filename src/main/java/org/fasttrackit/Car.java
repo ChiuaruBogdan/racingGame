6f0b0b6943991e84253ef2e65ae57bf4a3915de4
Chiuaru Bogdan <bchiuaru94@gmail.com>
2019-06-19 21:24:51 +0300

    Studied method and constructor "Overloading" - am adaugat un mesaj in clasa Car

--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -10,4 +10,10 @@ public class Car extends AutoVehicle{
     public Car(Engine engine) {
         super(engine);
     }
+
+
+    public Car() {
+//        any constructor will try to call the "no-parameter" constructor of the parent class
+//        (adica nu mai trebuie invocat 'super' din cauza la constructor overloading
+    }
 }
